50b6f2cf61acdee1263ebd0f499a63ab83a7e8a8
Tom Bell <tomb@tomb.io>
2022-11-02 23:04:05 +0000

    Remove hacks to redo them if needed

--- core.lua
+++ core.lua
@@ -11,12 +11,12 @@ local backdrop_tab = {
 }
 
 local powerColors = {
-  [0]  = cfg.colors.mana,
-  [1]  = cfg.colors.rage,
-  [2]  = cfg.colors.focus,
-  [3]  = cfg.colors.energy,
-  [6]  = cfg.colors.runicpower,
-  [8]  = cfg.colors.lunarpower,
+  [0] = cfg.colors.mana,
+  [1] = cfg.colors.rage,
+  [2] = cfg.colors.focus,
+  [3] = cfg.colors.energy,
+  [6] = cfg.colors.runicpower,
+  [8] = cfg.colors.lunarpower,
   [11] = cfg.colors.maelstrom,
   [13] = cfg.colors.insanity,
   [17] = cfg.colors.fury,
@@ -50,41 +50,26 @@ end
 local energyBarText = energyBar:CreateFontString(nil, "OVERLAY")
 energyBarText:SetFont(cfg.text.font, cfg.text.size, "THINOUTLINE")
 energyBarText:SetPoint(cfg.text.pos.a1, cfg.text.pos.af, cfg.text.pos.a2, cfg.text.pos.x, cfg.text.pos.y)
-energyBarText:SetJustifyH("RIGHT")
+energyBarText:SetJustifyH "RIGHT"
 
 local abs, max, min = math.abs, math.max, math.min
 energyBar:SetScript("OnUpdate", function(self, elapsed)
-  local maxPower = UnitPowerMax("player", UnitPowerType("player"))
-  local _, class = UnitClass("player")
-
-  -- hack to fix vengeance dh pain
-  if maxPower == 1000 and class == "DEMONHUNTER" then
-    maxPower = maxPower / 10
-  end
-
-  -- hack to fix death knight runic power
-  if class == "DEATHKNIGHT" then
-    if maxPower == 1000 or maxPower == 1150 then
-      maxPower = maxPower / 10
-    end
-  end
-
-  -- hack to fix shadow priest insanity
-  if maxPower == 10000 and class == "PRIEST" then
-    maxPower = maxPower / 100
-  end
+  local maxPower = UnitPowerMax("player", UnitPowerType "player")
+  local _, class = UnitClass "player"
 
   self:SetMinMaxValues(0, maxPower)
-  self:SetValue(UnitPower("player"))
+  self:SetValue(UnitPower "player")
 
   local limit = 30 / GetFramerate()
 
   for bar, value in pairs(smoothing) do
     local cur = bar:GetValue()
     local barmin, barmax = bar:GetMinMaxValues()
-    local new = cur + min((value-cur)/10, max(value-cur, limit))
+    local new = cur + min((value - cur) / 10, max(value - cur, limit))
 
-    if new ~= new then new = value end
+    if new ~= new then
+      new = value
+    end
 
     bar:SetValue_(new)
 
@@ -95,12 +80,12 @@ energyBar:SetScript("OnUpdate", function(self, elapsed)
   end
 end)
 
-energyBar:RegisterEvent("PLAYER_ENTERING_WORLD")
-energyBar:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
-energyBar:RegisterEvent("UNIT_AURA")
-energyBar:RegisterEvent("UNIT_POWER_FREQUENT")
+energyBar:RegisterEvent "PLAYER_ENTERING_WORLD"
+energyBar:RegisterEvent "PLAYER_SPECIALIZATION_CHANGED"
+energyBar:RegisterEvent "UNIT_AURA"
+energyBar:RegisterEvent "UNIT_POWER_FREQUENT"
 energyBar:SetScript("OnEvent", function(self, event, ...)
-  local powerColor = powerColors[UnitPowerType("player")]
+  local powerColor = powerColors[UnitPowerType "player"]
   energyBar:SetStatusBarColor(unpack(powerColor))
-  energyBarText:SetText(UnitPower("player"))
+  energyBarText:SetText(UnitPower "player")
 end)
