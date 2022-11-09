6370acb432ed470e4c69c62f7c3ba837fe405b6f
Tom Bell <tomb@tomb.io>
2022-11-09 20:02:07 +0000

    Update width

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ local mediapath = "Interface\\AddOns\\" .. A .. "\\media\\"
 
 L.cfg = {
   height = 15,
-  width = 168,
+  width = 204,
 
   pos = { a1 = "CENTER", a2 = "CENTER", af = "UIParent", x = 0, y = -168 },
 
